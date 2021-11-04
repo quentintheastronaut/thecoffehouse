8122458f64fc53e04a282e548f6cb297e1960cf9
Blue-Pheasant <BluePheasant@users.noreply.github.com>
2021-11-04 20:35:51 +0700

    Add store function in admin/controllers/store

---
+++ admin/controllers/store/edit.php
@@ -0,0 +1,16 @@
+<!--
+    admin/store/edit.php
+-->
+<?php
+permission_user();
+permission_moderator();
+require_once('admin/models/shop.php');
+if (!empty($_POST)) {
+    category_uodate();
+}
+if (isset($_GET['cate_id'])) $cate_id = intval($_GET['cate_id']);
+else $cate_id = 0;
+$title = ($cate_id == 0) ? 'Add category' : 'Edit category';
+$category = get_a_record('categories', $cate_id);
+$nav_category = 'class="active open"';
+require('admin/views/shop/edit.php');
