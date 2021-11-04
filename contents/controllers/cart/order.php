dfcb4e70610599729522d2558303e024a402f95c
Blue-Pheasant <BluePheasant@users.noreply.github.com>
2021-11-04 15:13:58 +0700

    Process cart functions in contents/controllers/cart with six files: add.php, checkout.php, delete.php, destroy.php, index.php, order.php

---
+++ contents/controllers/cart/order.php
@@ -0,0 +1,13 @@
+<!--
+    contents/controllers/cart/order.php
+-->
+<?php
+$title = 'Order';
+$cart = cart_list();
+if (empty($cart)) {
+    header('location:.');
+}
+global $user_nav;
+if (isset($user_nav)) $user_login = get_a_record('users', $user_nav);
+//load view
+require('contents/views/cart/order.php');
