f44167f3c47bd25489d8b2d44210c5cc6bff15a9
Blue-Pheasant <BluePheasant@users.noreply.github.com>
2021-11-05 21:41:19 +0700

    â

--- controllers/StoreController.php
+++ controllers/StoreController.php
@@ -5,14 +5,6 @@
 
     class StoreController extends StoreModel {
         public function index() {
-            if (isset($_GET['id'])) {
-                $store_id = intval($_GET['id']);
-            } else show_404();
-            $category = get_a_record('categories', $store_id);
-            if (!$category) show_404();
-            $categories = get_all('categories', array(
-                'select' => 'id, category_name',
-                'order_by' => 'category_position ASC'
-            ));
+           
         }
     }
