b0aafe51c621fc2ddc9a09c0df7f6c66541ce7af
Blue-Pheasant <BluePheasant@users.noreply.github.com>
2021-11-06 15:01:31 +0700

    No change

--- controllers/FeedbackController.php
+++ controllers/FeedbackController.php
@@ -4,17 +4,5 @@
 */
     require_once('models/feedback.php');
     class FeedbackController extends FeedbackModel {
-        public function index() {
-            if (!empty($_POST)) {
-                feedback_add();
-            }
-            if (isset($_GET['product_id'])) $product_id = intval($_GET['product_id']);
-            else $product_id = 0;
-            $product = get_a_record('products', $product_id);
-            if (isset($user_nav)) {
-                $user_action = get_a_record('users', $user_nav);
-            }
-            $title = 'Send your feedback to our system';
-            require('views/feedback/index.php');
-        }
+       
     }
