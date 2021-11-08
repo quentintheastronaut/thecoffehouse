8b4358496fc2d12f5da9f46a125414ed62cd24d2
@hanh <ngmhah@gmail.com>
2021-11-08 21:05:57 +0700

    Update about.php

--- views/about.php
+++ views/about.php
@@ -1,1 +1,54 @@
-<h1>About Kaffee store</h1>
+<div class="about-us">
+	<div class="page-heading">
+        <img class="page-banner" src="/images/page-banner.png" alt="page-banner" />
+		<h1>About Kaffee store</h1>
+	</div>	
+
+    <div class="info-detail"> 
+        <p style="text-align:justify">
+            <span style="font-size:20px">
+                <span style="color:#804000">
+                    <strong>VỀ CHÚNG TÔI</strong>
+                </span>
+            </span>
+        </p>
+        <p>
+            Chúng tôi tin rằng từng sản phẩm trà và cà phê sẽ càng thêm hảo hạng khi được tạo ra từ sự phấn đấu không ngừng cùng niềm đam mê. Và chính kết nối dựa trên niềm tin, sự trung thực và tin yêu góp phần mang đến những nét đẹp trong văn hóa thưởng trà & cà phê ngày càng bay cao và vươn xa. Tiếp nối nỗ lực, từ một cửa hàng đầu tiên, đến nay, Phúc Long xây dựng hơn 80 cửa hàng trên khu vực TP.HCM, Bình Dương, Biên Hòa, Cần Thơ, Đà Nẵng, Nha Trang, Hà Nội phục vụ những thức uống tươi ngon từ trà và cà phê. Không dừng lại tại đó, chúng tôi tiếp tục định hướng phát triển mở rộng hệ thống cửa hàng trải dài từ Nam ra Bắc. Tăng độ phủ của sản phẩm đến tất cả các hệ thống: siêu thị, cửa hàng tiện lợi…
+        </p>
+        <p> 
+            Hoạt động với phương châm “Chất lượng khởi nguồn từ đam mê”, cùng nhau, chúng tôi đã và đang viết tiếp câu chuyện của một thương hiệu Việt giàu truyền thống, kết nối tinh túy của nhiều thập niên kinh nghiệm và không ngừng chắt lọc, làm mới mình trong bước chuyển thời gian để trở thành thương hiệu luôn gắn bó với nhiều thế hệ khách hàng.
+        </p>
+        <p style="text-align:justify">
+            <span style="font-size:20px">
+                <span style="color:#804000">
+                    <strong>TẦM NHÌN</strong>
+                </span>
+            </span>
+        </p>
+        <p>
+            
+        </p>
+
+        <p style="text-align:justify">
+            <span style="font-size:20px">
+                <span style="color:#804000">
+                    <strong>SỨ MỆNH</strong>
+                </span>
+            </span>
+        </p>
+        <p>
+
+        </p>
+
+        <p style="text-align:justify">
+            <span style="font-size:20px">
+                <span style="color:#804000">
+                    <strong>GIÁ TRỊ CỐT LÕI</strong>
+                </span>
+            </span>
+        </p>
+        <p>
+            Kaffee cam kết cung cấp sản phẩm chất lượng và dịch vụ tốt nhất cho khách hàng; Xây dựng môi trường làm việc chuyên nghiệp, năng động, sáng tạo và nhân văn, tạo điều kiện thu nhập cao và cơ hội phát triển công bằng; Hài hòa lợi ích doanh nghiệp với lợi ích xã hội, đóng góp tích cực vào các hoạt động hướng về cộng đồng, thể hiện tinh thần trách nhiệm đối với xã hội.
+        </p>
+    </div>
+ </div>
