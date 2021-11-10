f3318d418a69b3e2c8195a93cf41e0f9285aae22
Nhat Quan <quan260402@gmail.com>
2021-11-10 08:44:52 +0700

    fix login bug after merge login

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -23,7 +23,8 @@ $path = str_replace($_SERVER['DOCUMENT_ROOT'], "", $path);
     <link rel="stylesheet" href="/css/product_detail.css">
     <link rel="stylesheet" href="/css/normalize.css">
     <link rel="stylesheet" href="/css/cart.css">
-    
+    <link rel="stylesheet" href="/css/home.css">
+
     <title>Kaffee store</title>
 </head>
 
--- views/login.php
+++ views/login.php
@@ -1,46 +1,50 @@
 <link rel="stylesheet" href="/css/login.css">
 <div class="container">
-	<div class="d-flex justify-content-center h-100">
-		<div class="card">
-			<div class="card-header">
-				<h3>Sign In</h3>
-				<div class="d-flex justify-content-end social_icon">
-					<span><i class="fab fa-facebook-square"></i></span>
-					<span><i class="fab fa-google-plus-square"></i></span>
-					<span><i class="fab fa-twitter-square"></i></span>
-				</div>
-			</div>
-			<div class="card-body">
-				<form>
-					<div class="input-group form-group">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fas fa-user"></i></span>
-						</div>
-						<input type="text" class="form-control" placeholder="username">
-						
-					</div>
-					<div class="input-group form-group">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fas fa-key"></i></span>
-						</div>
-						<input type="password" class="form-control" placeholder="password">
-					</div>
-					<div class="row align-items-center remember">
-						<input type="checkbox">Remember Me
-					</div>
-					<div class="form-group">
-						<input type="submit" value="Login" class="btn float-right login_btn">
-					</div>
-				</form>
-			</div>
-			<div class="card-footer">
-				<div class="d-flex justify-content-center links">
-					Don't have an account?<a href="#">Sign Up</a>
-				</div>
-				<div class="d-flex justify-content-center">
-					<a href="#">Forgot your password?</a>
-				</div>
-			</div>
-		</div>
-	</div>
+    <form method="post" action="">
+        <div class="d-flex justify-content-center h-100">
+            <div class="card">
+                <div class="card-header">
+                    <h3>Đăng nhập</h3>
+                    <div class="d-flex justify-content-end social_icon">
+                        <span><i class="fab fa-facebook-square"></i></span>
+                        <span><i class="fab fa-google-plus-square"></i></span>
+                        <span><i class="fab fa-twitter-square"></i></span>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <form>
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-user"></i></span>
+                            </div>
+                            <input type="email" name="email" class="form-control" id="exampleInputEmail1"
+                                placeholder="Email" aria-describedby="emailHelp">
+
+                        </div>
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-key"></i></span>
+                            </div>
+                            <input type="password" name="password" class="form-control" placeholder="Mật khẩu"
+                                id="exampleInputPassword1">
+                        </div>
+                        <div class="row align-items-center remember">
+                            <input type="checkbox">Nhớ tài khoản
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn float-right login_btn">Đặng nhập</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="card-footer">
+                    <div class="d-flex justify-content-center links">
+                        Trở thành viên Kaffee store !<a href="/register">Đăng ký</a>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <a href="#">Quên mật khẩu ?</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
 </div>
