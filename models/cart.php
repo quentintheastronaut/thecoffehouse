28103426b1f3cb77f4d90ea14e28228c2f91825e
Nhat Quan <quan260402@gmail.com>
2021-11-17 22:57:07 +0700

    update Cart model

--- models/cart.php
+++ models/cart.php
@@ -3,37 +3,32 @@
 namespace app\models;
 
 use app\core\CartModel;
-use app\core\Database;
-use app\core\DBModel;
 
-class Cart extends DBModel
+class Cart extends CartModel 
 {
-    public string $id = '';
-    public string $customer_id = '';
-    public string $status = '';
-
-    public function __construct($id, $customer_id, $status)
+    public array $list;
+    public int $status;
+    
+    public function __construct()
     {
-        $this->id = $id;
-        $this->customer_id = $customer_id;
-        $this->status = $status;
+        $this->status = 0;
+        $this->list = [];
     }
-
+    
     public static function tableName(): string
     {
-        return 'cart';
+        return 'carts';
     }
 
     public function attributes(): array
     {
-        return ['id', 'customer_id', 'status'];
+        return ['list'];
     }
 
     public function labels(): array
     {
         return [
-            'id' => 'ID',
-            'customer_id' => 'Customer ID',
+            'list' => 'List',
             'status' => 'Status',
         ];
     }
@@ -43,25 +38,9 @@ class Cart extends DBModel
         return [];
     }
 
-    public static function create($id)
-    {
-        $cart = new Cart(uniqid(), $id, 'processing');
-        $cart->save();
-    }
-
     public function save()
     {
         return parent::save();
-    }
-
-    public static function findCart($id)
-    {
-        $cart = Cart::getCart($id);
-        if (count($cart) == 0) {
-            return false;
-        } else {
-            return true;
-        }
     }
 
     public function getDisplayInfo(): string
@@ -69,22 +48,4 @@ class Cart extends DBModel
         return $this->list . ' ' . $this->status;
     }
 
-
-    public static function getCart($id)
-    {
-        $list = [];
-        $db = Database::getInstance();
-        $req = $db->query("SELECT * FROM cart WHERE customer_id = '" . $id . "' AND status = 'processing'");
-
-
-        foreach ($req->fetchAll() as $item) {
-            $list[] = new Cart(
-                $item['id'],
-                $item['customer_id'],
-                $item['status']
-            );
-        };
-
-        return $list;
-    }
 }
